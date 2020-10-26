7931bacfbea76daacf1df58593e192aa3963bb05
Andy <learncodingwithandy@gmail.com>
2020-10-26 22:54:25 +0530

    Push Update Commit

---
+++ build/precache-manifest.4d2e7e5719d3c2d00eaa91e0c969c3c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc71b1d022ea3ab8fec77adf2a20f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5da9f0dcab04e8d8f3b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5da9f0dcab04e8d8f3b",
+    "url": "/static/js/main.b04836c2.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
